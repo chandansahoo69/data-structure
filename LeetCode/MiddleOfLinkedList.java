cdf8397f70466ad827ebc00aa8e83bd3d88c04ea
Chandan <sahooc029@gmail.com>
2021-01-07 12:30:16 +0530

    find middle of the list

---
+++ LeetCode/MiddleOfLinkedList.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class MiddleOfLinkedList {
+	 public ListNode middleNode(ListNode head) {
+	        ListNode slow = head;
+	        ListNode fast = head;
+	        while(fast != null && fast.next != null){
+	            fast = fast.next.next;
+	            slow = slow.next;
+	        }
+	        return slow;
+	    }
+}
